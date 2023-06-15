fd102cba0df17d1c468a820d2a6b907106e9ae20
Ugota Chisom Stephen <ugotachisomstephen@gmail.com>
2023-06-15 12:18:42 +0100

    this program is meant to give the answers to a number powered by another

---
+++ float_input.py
@@ -0,0 +1,6 @@
+#in this program we are gonna be doing some arithmetics
+
+num_a = float(input("enter a number: "))
+num_b = float(input("enter your second number: "))
+num_c = num_a ** num_b
+print(num_a, "to the power of ", num_b,  "will give you", num_c)
