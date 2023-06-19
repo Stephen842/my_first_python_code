4f3dad6d5fcfa8588cce7a431af30c88221f7a31
Ugota Chisom Stephen <ugotachisomstephen@gmail.com>
2023-06-20 00:04:11 +0100

    this is a task

---
+++ flowers.py
@@ -0,0 +1,11 @@
+plant = input("Enter flower name: ")
+
+if plant == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+    
+if plant == "spathiphyllum":
+    print("No, I want a big Spathiphyllum!")
+    
+else:
+    print("Spathiphyllum! Not ", plant + "!")
+
