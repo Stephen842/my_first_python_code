4953e850e39e1f05251dc2d909df1ed248c8e673
Ugota Chisom Stephen <ugotachisomstephen@gmail.com>
2023-07-24 12:34:14 +0100

    this program uses function to perform some basic arithmetic calculation on two interger number

---
+++ arith_function.py
@@ -0,0 +1,43 @@
+#this program uses function to perform some basic arithmetis between two interger values
+
+
+#this program give the sum of numbers by addtion
+def add():
+    addition = a + b
+    print("the addition of", a , "to", b, "is", addition)
+
+a = int(input("enter a value for a: "))
+b = int(input("enter a value for b: "))
+
+add()
+
+
+#this program gives the difference between numbers by dividing
+def sub():
+    subtraction = a - b
+    print("the difference between", a , "to", b, "is", subtraction)
+
+a = int(input("enter a value for a: "))
+b = int(input("enter a value for b: "))
+
+sub()
+
+#this program multiply two given numbers
+def mul():
+    multiply = a * b
+    print("the multiple of", a , "to", b, "is", multiply)
+
+a = int(input("enter a value for a: "))
+b = int(input("enter a value for b: "))
+
+mul()
+
+#this program divide two given numbers
+def div():
+    divide = a / b
+    print("the division of", a , "and", b, "is", divide)
+
+a = int(input("enter a value for a: "))
+b = int(input("enter a value for b: "))
+
+div()
