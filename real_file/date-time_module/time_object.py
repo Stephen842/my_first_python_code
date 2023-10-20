45b7122f6e20d4552e80d1a49193e1074a8b949f
stephen842 <ugotachisomstephen@gmail.com>
2023-10-20 11:18:57 +0100

    this is a time object fixed with my own desired time

---
+++ real_file/date-time_module/time_object.py
@@ -0,0 +1,9 @@
+from datetime import time
+
+t = time(23, 15, 45, 3500)
+
+print("Time:", t)
+print("Hour:", t.hour)
+print("Minute:", t.minute)
+print("Second:", t.second)
+print("Microsecond:", t.microsecond)
